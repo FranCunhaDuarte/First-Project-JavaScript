20b474a472e13fcf47c9f9c1fe3404df9aa1e5cf
Franco Cunha Duarte <117323044+FranCunhaDuarte@users.noreply.github.com>
2023-01-10 14:30:09 -0300

    Bugs fixed

--- main.js
+++ main.js
@@ -8,9 +8,7 @@ let option=0;
 mainMenu();
 
 function mainMenu(){
-    console.log("Amount in your wallet: "+wallet);
-    console.log("Amount in your cart: "+cart);
-    option=parseInt(prompt("WELCOME TO ''MYSTORE''\n\n\nMENU \n 1. Add products to cart. \n 2. Deposit money in your wallet. \n 3. Payment process"));
+    option=parseInt(prompt("WELCOME TO ''MYSTORE''\n\n\nWallet: $"+wallet+"\nCart: $"+cart+"\n\nMENU \n 1. Add products to cart. \n 2. Deposit money in your wallet. \n 3. Payment process"));
     if(option==1){
         buyProducts();
     } else{
@@ -29,28 +27,28 @@ function mainMenu(){
 
 function buyProducts(){
     if(option==1){
-        let product=parseInt(prompt("Enter a number for add a product: \n1. Nintendo ( $200 ) \n2. Apple Watch ( $150 ) \n3. iPhone 11 Pro Max ( $1000 )\n4. MacBook Air ( $1500 )\n\nEnter 5 for go back."));
+        let product=parseInt(prompt("Wallet: $"+wallet+"\nCart: $"+cart+"\n\n\nEnter a number for add a product: \n1. Nintendo ( $200 ) \n2. Apple Watch ( $150 ) \n3. iPhone 11 Pro Max ( $1000 )\n4. MacBook Air ( $1500 )\n\nEnter 5 for go back."));
         switch(product){
             case 1:
-                let amountNintendo=parseInt(prompt("Enter a amount of this product"));
+                let amountNintendo=parseInt(prompt("How many Nintendos do you want?"));
                 cart=cart+(amountNintendo*nintendo);
                 console.log("Total: "+cart);
                 buyProducts();
                 break;
             case 2:
-                let amountAppleWatch=parseInt(prompt("Enter a amount of this product"));
+                let amountAppleWatch=parseInt(prompt("How many Apple Watchs do you want?"));
                 cart=cart+(amountAppleWatch*appleWatch);
                 console.log("Total: "+cart);
                 buyProducts();
                 break;
             case 3:
-                let amountIphone11ProMax=parseInt(prompt("Enter a amount of this product"));
+                let amountIphone11ProMax=parseInt(prompt("How many Iphone 11 Pro Max do you want?"));
                 cart=cart+(amountIphone11ProMax*iphone11ProMax);
                 console.log("Total: "+cart);
                 buyProducts();
                 break;
             case 4:
-                let amountMacBookAir=parseInt(prompt("Enter a amount of this product"));
+                let amountMacBookAir=parseInt(prompt("How many MacBook Air do you want?"));
                 cart=cart+(amountMacBookAir*macbookAir);
                 console.log("Total: "+cart);
                 buyProducts();
@@ -60,13 +58,14 @@ function buyProducts(){
                 mainMenu();
                 break;
             default:
-                
+                alert("Invalid number. Please Check!");
+                buyProducts();
         }
     }
 }
 
 function addAmount(){
-    let amount=parseInt(prompt("Enter how much do you want to put in your wallet"));
+    let amount=parseInt(prompt("Wallet: $"+wallet+"\nCart: $"+cart+"\n\nEnter how much do you want to put in your wallet"));
     wallet=wallet+amount;
     mainMenu();
 }
@@ -100,6 +99,8 @@ function verify(){
         switch(confirm){
             case 1:
                 alert("Thank you for trusting ''MYSTORE''. \nWithin 5 days your products will arrive at your doorstep.");
+                wallet=wallet-cart;
+                cart=0;
                 mainMenu();
                 break;
             case 2:
